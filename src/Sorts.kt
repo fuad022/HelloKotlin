0b92a454ff940703dac3db17e33dde59eb04dff7
f.r.malikov <fuad.malikov@rabitabank.com>
2022-05-05 09:00:41 +0400

    HelloKotlin project.

--- src/Sorts.kt
+++ src/Sorts.kt
@@ -106,13 +106,20 @@ fun quickSort(items: List<Int>): List<Int> {
     if (items.count() < 2) {
         return items
     }
+
     val pivot = items[items.count() / 2]
 
-    val equal = items.filter { it == pivot }
+    val equal = items.filter {
+        it == pivot
+    }
 
-    val less = items.filter { it < pivot }
+    val less = items.filter {
+        it < pivot
+    }
 
-    val greater = items.filter { it > pivot }
+    val greater = items.filter {
+        it > pivot
+    }
 
     return quickSort(less) + equal + quickSort(greater)
 }
